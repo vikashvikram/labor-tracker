2c50f80bd28cc910a685665cd8b8c7fe84f68922
Vikash Vikram <vikashvikram@gmail.com>
2015-06-05 12:34:51 +0530

    adding push_for_demo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,12 +12,53 @@ class WelcomeController < ApplicationController
   end
 
   def push_chart(xVal = "1", yVal = "1")
-  	xVal = 75
+  	xVal = 0
   	loop do
       puts xVal
   		Pusher.trigger('heartbeat', 'beat', {:value => xVal})
-  		xVal = 60+Random.rand(100)
+  		xVal = 80+Random.rand(100)
   		sleep 0.5
   	end
+  end
+
+  def push_for_demo
+    xVal = 80
+    sleep_time = 0.01
+    loop do
+      puts xVal
+      Pusher.trigger('heartbeat', 'beat', {:value => xVal})
+      sleep sleep_time
+      puts xVal+20
+      Pusher.trigger('heartbeat', 'beat', {:value => xVal+20})
+      sleep sleep_time
+      puts xVal
+      Pusher.trigger('heartbeat', 'beat', {:value => xVal})
+      sleep sleep_time
+      puts xVal
+      Pusher.trigger('heartbeat', 'beat', {:value => xVal})
+      sleep sleep_time
+      puts xVal
+      Pusher.trigger('heartbeat', 'beat', {:value => xVal})
+      sleep sleep_time
+      puts xVal - (10+Random.rand(10))
+      Pusher.trigger('heartbeat', 'beat', {:value => (xVal - (10+Random.rand(10)))})
+      sleep sleep_time
+      puts xVal+50+Random.rand(20)
+      Pusher.trigger('heartbeat', 'beat', {:value => (xVal+50+Random.rand(20))})
+      sleep sleep_time
+      puts xVal - (10+Random.rand(10))
+      Pusher.trigger('heartbeat', 'beat', {:value => xVal - (10+Random.rand(10))})
+      sleep sleep_time
+      puts xVal
+      Pusher.trigger('heartbeat', 'beat', {:value => xVal})
+      sleep sleep_time
+      puts xVal
+      Pusher.trigger('heartbeat', 'beat', {:value => xVal})
+      sleep sleep_time
+      puts xVal
+      Pusher.trigger('heartbeat', 'beat', {:value => xVal})
+      sleep sleep_time
+      #Pusher.trigger('heartbeat', 'beat', {:value => xVal})
+    end
   end
 end
